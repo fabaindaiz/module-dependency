5ab1b4e7ffa6475034cc2e627991cda8dcc54207
fabaindaiz <fabiandiaz@ug.uchile.cl>
2026-03-16 17:16:44 -0300

    fix fallback is debug log

--- src/dependency/core/agrupation/fallback.py
+++ src/dependency/core/agrupation/fallback.py
@@ -14,11 +14,11 @@ def initialize_fallback(parent: containers.Container) -> type[Plugin]:
         def initialize_fallback(cls, parent: containers.Container) -> None:
             #for container in Registry.containers:
             #    if container.parent is None and not container.is_root:
-            #        _logger.warning(f"Container {container} has been registered to fallback module")
+            #        _logger.debug(f"Container {container} has been registered to fallback module")
             #        container.change_parent(cls.injection)
             for provider in Registry.providers:
                 if provider.parent is None and not provider.is_root:
-                    _logger.warning(f"Provider {provider} has been registered to fallback module")
+                    _logger.debug(f"Provider {provider} has been registered to fallback module")
                     provider.injectable.strict_resolution = False
                     provider.change_parent(cls.injection)
             cls.resolve_providers(container=parent)
